e8caadbed93260e076b5814787f9bb1312da2d79
jc <2934123238@qq.com>
2022-07-30 14:18:58 +0800

    //2022-07-30
    3 join
    
    4 sleep
    
    5 线程中断 interrupt isInterrupted interrupted
    
    6 环形链死锁

---
+++ chapter1/src/DemonTest.java
@@ -0,0 +1,16 @@
+/**
+ * 设置为守护线程
+ */
+public class DemonTest {
+    public static void main(String[] args) {
+        Thread daemonThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        // 设置为守护线程
+        daemonThread.setDaemon(true);
+
+    }
+}
